afa041db3eb1c01053981a3c58ee34b9762f6262
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-22 16:21:23 -0300

    Implementa o teste que exibe -No favorite pokemon found-, se a pessoa não tiver pokémons favoritos.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Exibe No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => {
+    renderWithRouter(<App />);
+
+    const favPokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favPokemonsLink);
+
+    const noFavoriteMsg = screen.getByText('No favorite pokemon found');
+    expect(noFavoriteMsg).toBeInTheDocument();
+  });
+
+  it('', () => {
+
+  });
+});
