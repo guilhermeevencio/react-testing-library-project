07f3da7eeafce6df14f3856201d5afa98b7b4982
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-22 15:27:24 -0300

    Implementa todos os requisitos pedidos para realizar os testes do requisito 1.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../helpers/renderWithRouter';
 
 describe('O topo da aplicação contém um conjunto fixo de links de navegação.', () => {
   it('O primeiro link deve possuir o texto Home.', () => {
     renderWithRouter(<App />);
+
     const homeLink = screen.getByRole('link', { name: 'Home' });
     expect(homeLink).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto About.', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons.', () => {
+    renderWithRouter(<App />);
+
+    const favPokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favPokemonsLink).toBeInTheDocument();
+  });
+
+  it('A aplicação é redirecionada para a página inicial, na URL /.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('A aplicação é redirecionada para a About, na URL /about.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('A aplicação é redirecionada para Pokémons Favoritados, na URL /favorites', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favPokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favPokemonsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
   });
 });
