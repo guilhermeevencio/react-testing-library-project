64823d1d4b0e2814fb6fdfe02278ba4441a23e49
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-22 15:07:10 -0300

    Cria a função helper renderWithRouter e implementa o teste que verifica se o primeiro link possui o texto Home.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('O topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+  it('O primeiro link deve possuir o texto Home.', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+  });
+});
