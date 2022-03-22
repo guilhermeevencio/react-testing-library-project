15ec5e2421e3c10cf32e7f37366fb01eeb0c3f13
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-22 17:53:49 -0300

    Implementa o teste que verifica se os pokemons favoritados estão sendo renderizados na tela, concluindo o que foi pedido no requisito 03

---
+++ src/helpers/mockAPI.js
@@ -0,0 +1,5 @@
+export default function pokemonArr() {
+  return (
+    'oi'
+  );
+}
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -3,7 +3,9 @@ import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../helpers/renderWithRouter';
+import pokemons from '../data';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   it('Exibe No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => {
@@ -16,7 +18,13 @@ describe('Teste o componente <FavoritePokemons.js />', () => {
     expect(noFavoriteMsg).toBeInTheDocument();
   });
 
-  it('', () => {
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const favPokemons = [pokemons[0], pokemons[1]];
+    const { history } = renderWithRouter(<FavoritePokemons pokemons={ favPokemons } />);
+    history.push('/favorites');
 
+    favPokemons.forEach(({ name }) => {
+      expect(screen.getByText(name)).toBeInTheDocument();
+    });
   });
 });
