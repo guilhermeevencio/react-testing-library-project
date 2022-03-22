b00894fd5bc8a2a57dba9511fdb99a7f4cd27294
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-22 18:29:16 -0300

    Implementa o teste que verifica se o texto ->Page requested not found<- foi renderizado na página.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../helpers/renderWithRouter';
+import pokemons from '../data';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/not-find-this-route');
+
+    const notFoundText = screen.getByText('Page requested not found');
+    expect(notFoundText).toBeInTheDocument();
+  });
+});
