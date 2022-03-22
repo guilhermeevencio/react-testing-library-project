ed8b7f84ddc7ffc77cb6964f7e93cd663ef12bbd
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-22 16:03:09 -0300

    Realiza os testes para ver se existem os paragrefos que descrevem a pokedex e também o teste que verifica a existencia de um heading com o texto About Pokedex.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,40 @@
-test('teste', () => {});
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Testes do componente <About.js />.', () => {
+  it('Teste se a página contém as informações sobre a Pokédex.', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    const pokeInfo = screen.getByText(/a digital encyclopedia containing all pokémons/i);
+    expect(pokeInfo).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém as informações sobre a Pokédex(2 paragrafo).', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+    const pokeInfo2 = screen.getByText(/one can filter pokémons by type/i);
+    expect(pokeInfo2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+
+    const headingText = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(headingText).toBeInTheDocument();
+  });
+
+  it('Teste se a url da imagem da pokedex está correta', () => {
+    renderWithRouter(<App />);
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const image = screen.getByAltText('Pokédex');
+    expect(image).toHaveAttribute('src', url);
+  });
+});
