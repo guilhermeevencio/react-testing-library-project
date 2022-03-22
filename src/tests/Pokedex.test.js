f882eb8bae902dbdf653c8661ef9bf5a92ab8443
Guilherme <guilhermeevencioluz@gmail.com>
2022-03-22 19:05:35 -0300

    Implementa o teste que verifica a presença do heading com o texto ->Encountered pokémons<- no componente Pokedex.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen } from '@testing-library/react';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from '../helpers/renderWithRouter';
+import pokemons from '../data';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ {} } />,
+    );
+    const headingPokedex = screen.getByRole(
+      'heading', { level: 2, name: 'Encountered pokémons' },
+    );
+    expect(headingPokedex).toBeInTheDocument();
+  });
+});
